d3065cc395c9277bdd52a81d6073b627f3ac854b
August Miller <hello@gusmiller.com>
2015-12-29 17:38:50 -0800

    It exists

---
+++ lib/helpers.rb
@@ -0,0 +1,19 @@
+module Oof
+  def markdown(text)
+    Tilt::KramdownTemplate.new { text }.render
+  end
+
+  def tour
+    sitemap.find_resource_by_path('/tour/index.html')
+  end
+
+  def get_page(slug = false)
+    if slug
+      # Find the page in our pages folder
+      sitemap.find_resource_by_path "/pages/#{slug}/index.html"
+    else
+      # Return pages root
+      sitemap.find_resource_by_path "/pages/index.html"
+    end
+  end
+end
---
+++ source/index.html.md
@@ -0,0 +1,9 @@
+---
+title: oof. Studio
+---
+
+# oof.
+
+A new interdisciplinary studio in Portland, OR.
+
+We design and build interactive experiences for clients that share our committment to knowledge, opportunity, and human well-being.
